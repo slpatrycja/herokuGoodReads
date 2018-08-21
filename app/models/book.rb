f3163976a3841202674903523828f850f5f58b85
slpatrycja <sl.patrycja.of@gmail.com>
2018-08-21 10:16:44 +0200

    create search functionality

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,6 @@ class Book < ApplicationRecord
   def self.search(search)
     where("title LIKE ?  OR title LIKE ? OR title LIKE ? OR author LIKE ?",
       "%#{search.capitalize}%", "%#{search}%", "%#{search.downcase}%",
-       "%#{search.capitalize}%") 
+       "%#{search.capitalize}%")
   end
 end
